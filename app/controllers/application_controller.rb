9a4617ff94c8c1330a7772409c63245511289fee
sucatman5555 <sucatman5555@gmail.com>
2021-01-29 12:29:52 +0900

    オリジナルアプリ実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  # Basic認証の実装01
+  before_action :basic_auth
+  # //Basic認証の実装01
+  
   # 2020/12/25 add S.Shimada application_controllerに、emailとpassword以外の値も保存できるように追記する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -10,4 +14,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
   end
   # //2020/12/25 add S.Shimada application_controllerに、emailとpassword以外の値も保存できるように追記する
+
+  # Basic認証の実装02
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      # 環境変数を読み込む記述に変更
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+  # //Basic認証の実装02
 end
