7abf4d05cb136e4efc889a1808a2a3a59fc3dc59
sucatman5555 <sucatman5555@gmail.com>
2021-01-31 18:52:16 +0900

    不要な処理の削除（JS）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,22 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// 以下の３つを追記
-//= require jquery3
-//= require popper
-//= require bootstrap
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../SpeechSynthesis")
-
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-require("../sky")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
